0776ebb4d83d9a19e1f00282c1d46ea495f7912d
wfalcon <wfalcon@mail.ru>
2017-04-29 09:25:26 +0300

    Добавил Favicon и отредактирова зависимости в package.json

--- server.js
+++ server.js
@@ -4,11 +4,15 @@
 var express = require('express');
 var app = express();
 var colors = require('colors');
+var favicon = require('serve-favicon');
+
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname));
 
+app.use(favicon(__dirname + '/favicon.ico'));
+
 app.listen(app.get('port'), function() {
-    console.log(colors.green('Server started: http://localhost:' + app.get('port') + '/'));
+    console.log(colors.green.underline('Server started: http://localhost:' + app.get('port') + '/'));
 });
