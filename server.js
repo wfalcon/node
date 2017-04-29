447e7bf5e677348ee204d7b31acbf4a05b0437ca
wfalcon <wfalcon@mail.ru>
2017-04-29 08:52:09 +0300

    Прикрутил цветной вывод console.log

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
  */
 var express = require('express');
 var app = express();
+var colors = require('colors');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname));
 
 app.listen(app.get('port'), function() {
-    console.log('Server started: http://localhost:' + app.get('port') + '/');
+    console.log(colors.green('Server started: http://localhost:' + app.get('port') + '/'));
 });
