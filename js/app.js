11e095a08d26331e4666b080708b7cb7ad0373f9
wfalcon <wfalcon@mail.ru>
2017-04-30 15:13:36 +0300

    Добавил "подробнее" в новостях

--- js/app.js
+++ js/app.js
@@ -5,19 +5,18 @@ var my_news1 = [];
 var my_news = [
     {
         author: 'Саша Печкин',
-        text: 'В четверг, четвертого числа...'
+        text: 'В четчерг, четвертого числа...',
+        bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
     },
     {
         author: 'Просто Вася',
-        text: 'Считаю, что $ должен стоить 35 рублей!'
+        text: 'Считаю, что $ должен стоить 35 рублей!',
+        bigText: 'А евро 42!'
     },
     {
         author: 'Гость',
-        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
-    },
-    {
-        author: 'Falcon',
-        text: 'Сегодня хороший день чтобы учиться'
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+        bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
     }
 ];
 
@@ -61,13 +60,33 @@ var News = React.createClass({
 });
 
 var Article = React.createClass({
+    propType: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function() {
+        return {
+            visible: false
+        };
+    },
+    readmoreClick: function(e) {
+        e.preventDefault();
+        this.setState({visible: true});
+    },
     render: function () {
         var author = this.props.data.author,
-            text = this.props.data.text;
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
         return (
             <div className="article">
                 <p className="news__author">{author}</p>
                 <p className="news__text">{text}</p>
+                <a href="#" onClick={this.readmoreClick} className={"news__readmore " + (visible ? 'none': '')}>Подробнее</a>
+                <p className={"news__big_text " + (visible ? '' : 'none')}>{bigText}</p>
             </div>
         )
     }
